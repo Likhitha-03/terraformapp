16c91a937f455df2da3817d7c08982616fddc770
Likhitha-03 <86105341+Likhitha-03@users.noreply.github.com>
2024-09-12 14:49:10 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0182f373e66f89c85"  
+  ami           = "ami-0182f373e66f89c85"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
-  security_groups = [aws_security_group.web_sg.name]
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "runner"
+    Name = "docker-web-app"
   }
 }
 
