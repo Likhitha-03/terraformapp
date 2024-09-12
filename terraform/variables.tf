07bd21790b36d8ed93d2907d884d505256399ffa
Likhitha-03 <86105341+Likhitha-03@users.noreply.github.com>
2024-09-12 15:19:23 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,24 @@
+variable "vpc_id" {
+  description = "The ID of the existing VPC"
+  type        = string
+  default     = "vpc-0ecd36c175ff7f71f"
+}
+
+variable "subnet_id" {
+  description = "The ID of the existing subnet"
+  type        = string
+  default     = "subnet-0fced310d34fde07b"
+}
+
+variable "security_group_id" {
+  description = "The ID of the existing security group"
+  type        = string
+  default     = "sg-0bbc5e6ba111a1bbf"
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"
+}
 
