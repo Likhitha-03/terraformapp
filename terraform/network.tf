c47e07b22448bbabc99a8c45ac3164c7807f161f
Likhitha-03 <86105341+Likhitha-03@users.noreply.github.com>
2024-09-12 14:16:28 +0530

    Create network.tf

---
+++ terraform/network.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraformapp-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Change to your preferred AZ
+  tags = {
+    Name = "terraformapp-subnet"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "terraform-security-group"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
