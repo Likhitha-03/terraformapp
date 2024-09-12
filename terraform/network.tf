868f9a149a33cd2548b6550f2b7747eeb0f62119
Likhitha-03 <86105341+Likhitha-03@users.noreply.github.com>
2024-09-12 15:22:55 +0530

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,35 +1,11 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "terraformapp-vpc"
-  }
+data "aws_vpc" "existing" {
+  id = var.vpc_id
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  
-  tags = {
-    Name = "terraformapp-subnet"
-  }
+data "aws_subnet" "existing" {
+  id = var.subnet_id
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "terraform-security-group"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "existing" {
+  id = var.security_group_id
 }
